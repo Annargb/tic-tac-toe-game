ce036f605b0d4c5b55691360859cc3c90f5c94de
Annargb <annasamoylenko2015@gmail.com>
2024-01-02 23:11:06 +0100

    add sounds

--- src/js/index.js
+++ src/js/index.js
@@ -1,88 +1,7 @@
-// const content = document.querySelector('.js-content');
-// // const container = document.querySelector('.js-container');
-
-// let player = 'X';
-// // let player = '';
-// let historyX = [];
-// let historyO = [];
-// const wins = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8, 9],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [3, 6, 9],
-//   [1, 5, 9],
-//   [3, 5, 7],
-// ];
-
-// function createMarkup() {
-//   let markup = '';
-
-//   for (let i = 1; i < 10; i += 1) {
-//     markup += `<div class="item js-item" data-id="${i}"></div>`;
-//   }
-//   content.innerHTML = markup;
-// }
-// createMarkup();
-
-// content.addEventListener('click', onClick);
-
-// function onClick(event) {
-//   const { target } = event;
-//   const id = Number(target.dataset.id);
-//   let result = false;
-//   target.textContent = player;
-
-//   console.log(player);
-//   // if (!target.classList.contains('js-item') || target.textContent !== '') {
-//   //   return;
-//   // }
-//   if (!target.classList.contains('js-item')) {
-//     return;
-//   }
-
-//   if (player === 'X') {
-//     historyX.push(id);
-//     result = isWinner(historyX);
-//   } else {
-//     historyO.push(id);
-//     result = isWinner(historyO);
-//   }
-
-//   // target.textContent = player;
-
-//   if (result) {
-//     console.log(`Winner ${player}`);
-//     resetGame();
-//     return;
-//   } else if (historyX.length + historyO.length === 9) {
-//     console.log('No winner');
-//     resetGame();
-//     return;
-//   }
-
-//   player = player === 'X' ? 'O' : 'X';
-//   // if (target.textContent === '') {
-//   //   player = player === 'X' ? 'O' : 'X';
-//   // }
-//   // target.textContent = player;
-//   // if (target.textContent) {
-//   //   return;
-//   // }
-//   // player = player === 'X' ? 'O' : 'X';
-// }
-
-// function isWinner(arr) {
-//   return wins.some(item => item.every(num => arr.includes(num)));
-// }
-
-// function resetGame() {
-//   createMarkup();
-//   historyX = [];
-//   historyO = [];
-//   player = 'X';
-// }
+const moveUrl = require('url:../sounds/move.mp3');
+const winnerUrl = require('url:../sounds/success.mp3');
+const loserUrl = require('url:../sounds/loser.mp3');
+const drawUrl = require('url:../sounds/draw.mp3');
 
 const board = document.querySelector('.js-board');
 const title = document.querySelector('.js-title');
@@ -91,16 +10,39 @@ const counterTotal = document.querySelector('.js-counter-total');
 const counterTextX = document.querySelector('.js-counter-x');
 const counterTextO = document.querySelector('.js-counter-o');
 const counterTextDraw = document.querySelector('.js-counter-draw');
+// const timeX = document.querySelector('.time-x');
+// // const timeO = document.querySelector('.time-o');
+// // const timeDraw = document.querySelector('.time-draw');
 
+const checkbox = document.querySelector('.checkbox');
+
+const winsCombination = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7],
+];
+
+const moveSound = new Audio(moveUrl);
+const winnerSound = new Audio(winnerUrl);
+const loserSound = new Audio(loserUrl);
+const drawSound = new Audio(drawUrl);
+
+let isActiveSound = true;
 let currentPlayer = 'X';
 let historyX = [];
 let historyO = [];
 let cells = [];
 let cell = '';
 let gameOver = false;
-let counterX = { value: 0 };
-let counterO = { value: 0 };
-let counterDraw = { value: 0 };
+
+const counterX = { value: 0 };
+const counterO = { value: 0 };
+const counterDraw = { value: 0 };
 
 function createMarkup() {
   for (let i = 1; i < 10; i += 1) {
@@ -132,48 +74,79 @@ function onClick(event) {
   ) {
     target.textContent = currentPlayer;
     historyX.push(id);
+    checkIsActiveSound(moveSound);
   }
 
   checkIsWinner();
+  paintWinner(historyX);
 
   if (!gameOver) {
     setTimeout(makeBotMove, 500);
   }
 }
 
-function isWinner(arr) {
-  const winsCombination = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-  ];
+function checkIsActiveSound(sound) {
+  isActiveSound ? sound.play() : sound.pause();
+}
 
+function isWinner(arr) {
   return winsCombination.some(item => item.every(num => arr.includes(num)));
+}
+
+function paintWinner(history) {
+  for (const arr of winsCombination) {
+    const isWinningCombo = arr.every(num => history.includes(num));
+    if (isWinningCombo) {
+      for (const num of arr) {
+        const winningCell = cells.find(cell => Number(cell.dataset.id) === num);
+        winningCell.classList.add('winning-cell');
+      }
+      break;
+    }
+  }
 }
 
 function checkIsWinner() {
   if (isWinner(historyX)) {
     setWinner('Winner: X', counterX, counterTextX);
+    checkIsActiveSound(winnerSound);
   } else if (isWinner(historyO)) {
     setWinner('Winner: O', counterO, counterTextO);
+    checkIsActiveSound(loserSound);
   } else if (historyX.length + historyO.length === 9) {
     setWinner("It's a draw. Try again!", counterDraw, counterTextDraw);
+    checkIsActiveSound(drawSound);
   }
 
+  setTotalGames();
+}
+
+function setTotalGames() {
   counterTotal.textContent =
     counterX.value + counterO.value + counterDraw.value;
 }
 
+// function setWinner(str, counter, counterText, timeText) {
+//   title.textContent = str;
+//   counter.value += 1;
+//   counterText.textContent = counter.value;
+//   timeText.textContent = setWordTime(counter);
+//   gameOver = true;
+// }
+
 function setWinner(str, counter, counterText) {
   title.textContent = str;
   counter.value += 1;
-  counterText.textContent = counter.value;
+  counterText.textContent = `${counter.value} ${setWordTime(counter)}.`;
   gameOver = true;
+}
+
+function setWordTime(counter) {
+  if (counter.value === 1) {
+    return 'time';
+  } else {
+    return 'times';
+  }
 }
 
 function makeBotMove() {
@@ -184,6 +157,7 @@ function makeBotMove() {
     botCell.textContent = 'O';
     historyO.push(Number(botCell.dataset.id));
     checkIsWinner();
+    paintWinner(historyO);
   }
 }
 
@@ -195,6 +169,7 @@ function resetGame() {
   title.textContent = 'Who will win?';
   cells.forEach(cell => {
     cell.textContent = '';
+    cell.classList.remove('winning-cell');
   });
 }
 
@@ -202,4 +177,14 @@ button.addEventListener('click', onButtonClick);
 
 function onButtonClick() {
   resetGame();
+}
+
+checkbox.addEventListener('change', onChangeCheckbox);
+
+function onChangeCheckbox() {
+  if (checkbox.checked) {
+    isActiveSound = false;
+  } else {
+    isActiveSound = true;
+  }
 }
